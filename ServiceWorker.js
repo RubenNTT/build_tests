1597bf7916b71862db979818acc5b8b5158b8994
Ruben Parma <rparmaia@emeal.nttdata.com>
2024-07-08 15:16:13 +0200

    Release

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "DefaultCompany-ZapparTestURP-0.1.0";
 const contentToCache = [
     "Build/aca17784a298616c111c1b891787f0af.loader.js",
     "Build/ec2e2519a360384169e6bc2c39a68f64.framework.js",
-    "Build/61c62f2a4e93bd165af1202e9bba3314.data",
+    "Build/5c7a0e3ec7d087c58a60f8061a4f468e.data",
     "Build/01f77212f89ad97891e569c08c7a01ec.wasm",
     "TemplateData/style.css"
 
