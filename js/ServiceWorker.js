f701867f7ea375effb2f7a0cb9caa8b4cc6efc06
Ruben Parma <rparmaia@emeal.nttdata.com>
2024-07-19 11:31:39 +0200

    Release

--- js/ServiceWorker.js
+++ js/ServiceWorker.js
@@ -2,8 +2,8 @@ const cacheName = "CUPRA-CUPRA America's Cup-0.1";
 const contentToCache = [
     "Build/app.loader.js",
     "Build/b480e04c8cae7e08b41054d1af6bfb06.js",
-    "Build/87d01a52b6b4a0e12d14c76217ed6a29.data",
-    "Build/cd8f815bd21f83ca87d7eca6feed296a.wasm",
+    "Build/e67ab663043cb90914b9e69eefcf4455.data",
+    "Build/98f02c3d5b60be0e50cc762ae4ee155b.wasm",
     "TemplateData/style.css"
 
 ];
