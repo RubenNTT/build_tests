f705b06fb6225afc877c3b904e5e0d90f083c9e0
Ruben Parma <rparmaia@emeal.nttdata.com>
2024-07-24 14:54:22 +0200

    Release

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "CUPRA-CUPRA America's Cup-0.1";
+const contentToCache = [
+    "Build/app.loader.js",
+    "Build/71efb004b5e979d49a9dc26c090887c7.js",
+    "Build/c1ef2fdadd85b5b0108347f99224d262.data",
+    "Build/6c223cd92496b9f503406340f1c2c1e5.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+//      const cache = await caches.open(cacheName);
+//      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+//      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
