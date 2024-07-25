d8ddb4a516a14619af623d04e935d47e905f3a13
Ruben Parma <rparmaia@emeal.nttdata.com>
2024-07-25 13:00:33 +0200

    Release

---
+++ SCALE_AS_DEPTH_450px_200pt/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "CUPRA-CUPRA America's Cup-0.1";
+const contentToCache = [
+    "Build/SCALE_AS_DEPTH_450px_200pt.loader.js",
+    "Build/57abbecd2396f1cd8fc6cd52bce58110.js",
+    "Build/481e447219aa130af09925de83c9518d.data",
+    "Build/abcd38443eacdfe073dcd19cd6fbdea9.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+//      const cache = await caches.open(cacheName);
+//      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+//      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
---
+++ Z_AS_DEPTH_EXPERIMENTAL_450px_50pt/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "CUPRA-CUPRA America's Cup-0.1";
+const contentToCache = [
+    "Build/Z_AS_DEPTH_EXPERIMENTAL_450px_50pt.loader.js",
+    "Build/57abbecd2396f1cd8fc6cd52bce58110.js",
+    "Build/4c6a6e5cc9bdd31bd338eb92e94f49da.data",
+    "Build/abcd38443eacdfe073dcd19cd6fbdea9.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+//      const cache = await caches.open(cacheName);
+//      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+//      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
